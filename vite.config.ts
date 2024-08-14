399fe33e797cd53397aa4c3411fa6b280833a606
blyoa <9126033+blyoa@users.noreply.github.com>
2024-08-14 15:39:21 +0900

    build: extract license statements with rollup-plugin-license

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import licensePlugin from 'rollup-plugin-license';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    licensePlugin({
+      thirdParty: {
+        output: path.join(__dirname, 'dist', 'third-party-licenses.txt'),
+      },
+    }),
+  ],
 });
