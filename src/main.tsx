8bac4a9268ff577c81457c83d3ab635ea14e21d9
blyoa <9126033+blyoa@users.noreply.github.com>
2024-08-15 18:44:40 +0900

    feat: add Google Tag Manager settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TagManager from 'react-gtm-module';
 import { App } from './App.tsx';
 import './style.css';
+
+TagManager.initialize({
+  gtmId: 'GTM-P7HVP9K9',
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
