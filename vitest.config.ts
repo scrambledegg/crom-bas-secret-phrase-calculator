4d8ad395b23ec7dc1aaf1631bc5141dab8bc6e82
blyoa <9126033+blyoa@users.noreply.github.com>
2024-08-15 11:52:35 +0900

    build: add a script to run tests

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: ['./vitest.setup.ts'],
+
+    server: {
+      deps: {
+        inline: ['@adobe/react-spectrum', '@react-spectrum', '@spectrum-icons'],
+      },
+    },
+  },
+});
